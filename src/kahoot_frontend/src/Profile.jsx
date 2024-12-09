e3846e9f669cffc2920a72b80000748daf55e306
stanlys96 <stanlyskwok@gmail.com>
2024-12-09 22:51:22 +0700

    wlao

--- src/kahoot_frontend/src/Profile.jsx
+++ src/kahoot_frontend/src/Profile.jsx
@@ -5,6 +5,8 @@ import { useSelector } from "react-redux";
 
 function Profile() {
   const { principal } = useSelector((state) => state.user);
+  const [isHoveredKahoot, setIsHoveredKahoot] = useState(false);
+  const [isHoveredDraft, setIsHoveredDraft] = useState(false);
   return (
     <main className="background flex justify-center items-center">
       <div className="circle-bg" />
@@ -15,7 +17,31 @@ function Profile() {
         </div>
         <div className="flex gap-x-4 items-start">
           <div className="your-kahoots h-fit">
-            <div className="your-kahoots-top"></div>
+            <div className="your-kahoots-top">
+              <div className="flex gap-x-4">
+                <p
+                  onMouseEnter={() => setIsHoveredKahoot(true)}
+                  onMouseLeave={() => setIsHoveredKahoot(false)}
+                  className={`text-[#6E6E6E] ${
+                    isHoveredKahoot ? "text-container" : "text-ordinary"
+                  } cursor-pointer font-semibold kahoot-text`}
+                >
+                  Your kahoots
+                </p>
+                <p
+                  onMouseEnter={() => setIsHoveredDraft(true)}
+                  onMouseLeave={() => setIsHoveredDraft(false)}
+                  className={`text-[#6E6E6E] ${
+                    isHoveredDraft ? "text-container" : "text-ordinary"
+                  } cursor-pointer font-semibold kahoot-text`}
+                >
+                  Your drafts
+                </p>
+              </div>
+            </div>
+            <div>
+              <p className="text-black">ASDJKL</p>
+            </div>
           </div>
           <div className="kahoot-card cursor-pointer">
             <a className="kahoot-card-title">
