a4e9303235530f10a75867cc6da8839935bf1bdd
stanlys96 <stanlyskwok@gmail.com>
2024-12-11 13:33:25 +0700

    wlao

--- src/kahoot_frontend/src/Create.jsx
+++ src/kahoot_frontend/src/Create.jsx
@@ -1,17 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { motion } from "framer-motion";
-import { FaExclamationCircle } from "react-icons/fa";
-import { MdOutlineFolderCopy } from "react-icons/md";
+import { FaArrowRight, FaRegQuestionCircle } from "react-icons/fa";
+import { MdOutlineFolderCopy, MdQuiz, MdAccessTime } from "react-icons/md";
 import { RiDeleteBinLine } from "react-icons/ri";
+import { CiCircleQuestion } from "react-icons/ci";
+import { Dropdown, Space } from "antd";
+import { DownOutlined } from "@ant-design/icons";
+import { VscSymbolBoolean } from "react-icons/vsc";
+import { TiMessageTyping } from "react-icons/ti";
 
 function Create() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenExample, setIsOpenExample] = useState(false);
 
   const toggleModal = () => {
-    setIsOpen(!isOpen);
+    setIsOpenExample(!isOpenExample);
   };
+
+  const dropdownRef = useRef(null);
+
+  const items = [
+    {
+      label: (
+        <div className="the-gray rounded-[4px] p-[8px] flex flex-col gap-y-2 justify-center items-center">
+          <MdQuiz size="30px" />
+          <p className="text-black">Quiz</p>
+        </div>
+      ),
+      key: "0",
+    },
+    {
+      label: (
+        <div className="the-gray rounded-[4px] p-[8px] flex flex-col gap-y-2 justify-center items-center">
+          <VscSymbolBoolean size="30px" />
+          <p className="text-black">True or False</p>
+        </div>
+      ),
+      key: "1",
+    },
+    {
+      label: (
+        <div className="the-gray rounded-[4px] p-[8px] flex flex-col gap-y-2 justify-center items-center">
+          <TiMessageTyping size="30px" />
+          <p className="text-black">Type Answer</p>
+        </div>
+      ),
+      key: "3",
+    },
+  ];
+
   return (
-    <main className="background">
+    <main className="background" ref={dropdownRef}>
       <nav className="navbar">
         <div className="flex gap-x-6 items-center w-full">
           <img className="h-[48px]" src="/kahoot-2.png" />
@@ -31,57 +69,115 @@ function Create() {
           <button className="save-button">Save</button>
         </div>
       </nav>
-      <div className="the-sidebar">
-        <ul className="sidebar-ul">
-          <li className="sidebar-li">
-            <div className="sidebar-card relative">
-              <div>
-                <div className="flex gap-x-2">
-                  <p className="ml-[32px] text-sidebar">1</p>
-                  <p className="text-sidebar">Quiz</p>
+      <div className="flex justify-between">
+        <div className="the-sidebar">
+          <ul className="sidebar-ul">
+            <li className="sidebar-li">
+              <div className="sidebar-card relative">
+                <div>
+                  <div className="flex gap-x-2">
+                    <p className="ml-[32px] text-sidebar">1</p>
+                    <p className="text-sidebar">Quiz</p>
+                  </div>
+                  <div className="quiz-card ml-[32px] cursor-pointer mt-[5px] mx-[16px] relative">
+                    <p className="text-[0.75rem] my-[5px] text-center font-medium">
+                      Title
+                    </p>
+                    <div className="bg-[#F2F2F2] rounded-b-[0.25rem]">
+                      <img src="/cdn.svg" className="rounded-b-[0.25rem]" />
+                    </div>
+                    <div className="exclamation-container">
+                      <img className="exclamation" src="/exclamation.svg" />
+                    </div>
+                  </div>
                 </div>
-                <div className="quiz-card ml-[32px] cursor-pointer mt-[5px] mx-[16px] relative">
-                  <p className="text-[0.75rem] my-[5px] text-center font-medium">
-                    Title
-                  </p>
-                  <div className="bg-[#F2F2F2] rounded-b-[0.25rem]">
-                    <img src="/cdn.svg" className="rounded-b-[0.25rem]" />
-                  </div>
-                  <div className="exclamation-container">
-                    <img className="exclamation" src="/exclamation.svg" />
-                  </div>
+                <div className="flex flex-col gap-y-3 absolute top-[60%] left-[7px]">
+                  <a className="cursor-pointer">
+                    <MdOutlineFolderCopy
+                      color="#6E6E6E"
+                      height={16}
+                      width={16}
+                    />
+                  </a>
+                  <a className="cursor-pointer">
+                    <RiDeleteBinLine color="#6E6E6E" height={16} width={16} />
+                  </a>
                 </div>
               </div>
-              <div className="flex flex-col gap-y-3 absolute top-[60%] left-[7px]">
-                <a className="cursor-pointer">
-                  <MdOutlineFolderCopy color="#6E6E6E" height={16} width={16} />
-                </a>
-                <a className="cursor-pointer">
-                  <RiDeleteBinLine color="#6E6E6E" height={16} width={16} />
-                </a>
-              </div>
+            </li>
+          </ul>
+          <div className="sidebar-bottom">
+            <div className="question-container">
+              <span>
+                <button className="question-btn">Add question</button>
+              </span>
             </div>
-          </li>
-        </ul>
-        <div className="sidebar-bottom">
-          <div className="question-container">
-            <span>
-              <button className="question-btn">Add question</button>
-            </span>
+          </div>
+        </div>
+        <div className="middle-component flex-1 bg-black"></div>
+        <div className="right-sidebar">
+          <button className="right-sidebar-btn text-black">
+            <FaArrowRight className="absolute left-[20%]" />
+          </button>
+          <div className="inner-right-sidebar w-full">
+            <div className="flex gap-x-2 items-center">
+              <FaRegQuestionCircle
+                className="w-[24px] h-[24px]"
+                color="black"
+              />
+              <p className="text-[#333333] font-semibold">Question Type</p>
+            </div>
+            <Dropdown
+              className="dropdown-sidebar mt-3 w-full text-black"
+              menu={{ items }}
+              trigger={["click"]}
+            >
+              <a
+                className="flex justify-between items-center px-[10px]"
+                onClick={(e) => e.preventDefault()}
+              >
+                <div className="flex items-center gap-x-2">
+                  <MdQuiz />
+                  <p className="text-black">Quiz</p>
+                </div>
+                <DownOutlined />
+              </a>
+            </Dropdown>
+            <div className="horizontal-rule" />
+            <div className="flex gap-x-2 items-center">
+              <MdAccessTime className="w-[24px] h-[24px]" color="black" />
+              <p className="text-[#333333] font-semibold">Time Limit</p>
+            </div>
+            <Dropdown
+              className="dropdown-sidebar mt-3 w-full text-black"
+              menu={{ items }}
+              trigger={["click"]}
+            >
+              <a
+                className="flex justify-between items-center px-[10px]"
+                onClick={(e) => e.preventDefault()}
+              >
+                <div className="flex items-center gap-x-2">20 seconds</div>
+                <DownOutlined />
+              </a>
+            </Dropdown>
+          </div>
+          <div className="right-sidebar-bottom mt-auto">
+            <p className="text-black">Asd</p>
           </div>
         </div>
       </div>
 
       {/* Button to open modal */}
-      <button
+      {/* <button
         onClick={toggleModal}
         className="px-4 py-2 bg-blue-600 text-white rounded-md shadow-md hover:bg-blue-700 transition"
       >
         Open Modal
-      </button>
+      </button> */}
 
       {/* Modal Overlay */}
-      {isOpen && (
+      {isOpenExample && (
         <div
           className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
           onClick={toggleModal} // Close modal on background click
